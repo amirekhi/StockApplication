75387117cac8fb761477140df575ced72eddb59f
amir <skullbreaker44333@gmail.com>
2025-05-17 18:01:09 +0330

    Fixing Dependencie mismatches

--- obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3a070467d4d137f8e443fce986fd53fa49dd10b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+281728c0f460431bf0f9a40f4c53cea68a45bc62")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
